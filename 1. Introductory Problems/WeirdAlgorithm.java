f78966998da700534e9825492fc7a825d03b8138
Amartya Sinha <amartyasinha918@gmail.com>
2024-07-02 20:05:45 +0530

    change odd even logic using bitwise and

--- 1. Introductory Problems/WeirdAlgorithm.java
+++ 1. Introductory Problems/WeirdAlgorithm.java
@@ -1,33 +1,24 @@
 import java.util.Scanner;
 
-/**
- * WeirdAlgorithm
- */
+
 public class WeirdAlgorithm {
 
-    public static StringBuilder WeirdAlgorithmFunc(int num) {
+    public static StringBuilder WeirdAlgorithmFunc(long num) {
 
         StringBuilder result = new StringBuilder();
         result.append(num);
 
         while (num != 1) {
-            if (num % 2 == 1) {
-                num = 3 * num + 1;
-            }
-            else {
-                num /= 2;
-            }
+            num = ((num & 1) == 1) ? (3 * num + 1) : ( num / 2);
             result.append(" ").append(num);
         }
-
         return result;
     }
 
     public static void main(String[] args) {
 
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter your number: ");
-            int input = scanner.nextInt();
+            long input = scanner.nextLong();
             System.out.println(WeirdAlgorithmFunc(input));
         }
     }
