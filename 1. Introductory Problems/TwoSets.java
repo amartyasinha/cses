32f7215f06920d26478bbfb7ba47113c4e85ec1a
Amartya Sinha <amartya.sinha@sarathisoftech.com>
2024-07-03 17:55:19 +0530

    time limit exceed fix

--- 1. Introductory Problems/TwoSets.java
+++ 1. Introductory Problems/TwoSets.java
@@ -16,47 +16,56 @@ public class TwoSets {
 
     private static void MakeTwoSets(long n) {
         long totalSum = (n * (n + 1)) / 2;
-        if ((totalSum & 1) == 1){
-            System.out.println("NO");
-        }
-        else {
-            System.out.println("YES");
-            long maxNum = n;
-            boolean[] isUsed = new boolean[(int) n+1];
+        try (
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            if ((totalSum & 1) == 1){
+                System.out.println("NO");
+            }
+            else {
+                bw.write("YES");
+                bw.newLine();
+                long maxNum = n;
+                boolean[] isUsed = new boolean[(int) n+1];
 
-            List<Long> set1 = new ArrayList<>();
-            List<Long> set2 = new ArrayList<>();
+                List<Long> set1 = new ArrayList<>();
+                List<Long> set2 = new ArrayList<>();
 
-            long set1Sum = 0;
+                long set1Sum = 0;
 
-            while (set1Sum < totalSum/2) {
+                while (set1Sum < totalSum/2) {
 
-                long remainingSum = totalSum/2 - set1Sum;
-                if (remainingSum > maxNum) {
-                    set1Sum += maxNum;
-                    set1.add(maxNum);
-                    isUsed[(int) maxNum] = true;
-                    maxNum--;
-                } else {
-                    set1Sum += remainingSum;
-                    set1.add(remainingSum);
-                    isUsed[(int) remainingSum] = true;
+                    long remainingSum = totalSum/2 - set1Sum;
+                    if (remainingSum > maxNum) {
+                        set1Sum += maxNum;
+                        set1.add(maxNum);
+                        isUsed[(int) maxNum] = true;
+                        maxNum--;
+                    } else {
+                        set1Sum += remainingSum;
+                        set1.add(remainingSum);
+                        isUsed[(int) remainingSum] = true;
+                    }
+                }
+                for (long i = 1; i <= n; i++) {
+                    if (!isUsed[(int) i]) {
+                        set2.add(i);
+                    }
+                }
+                bw.write(String.valueOf(set1.size()));
+                bw.newLine();
+                for (long x : set1){
+                    bw.write(x + " ");
+                }
+                bw.newLine();
+                bw.write(String.valueOf(set2.size()));
+                bw.newLine();
+                for (long y : set2) {
+                    bw.write(y + " ");
                 }
             }
-            for (long i = 1; i <= n; i++) {
-                if (!isUsed[(int) i]) {
-                    set2.add(i);
-                }
-            }
-            System.out.println(set1.size());
-            for (long x : set1){
-                System.out.print(x + " ");
-            }
-            System.out.println();
-            System.out.println(set2.size());
-            for (long y : set2) {
-                System.out.print(y + " ");
-            }
+        } catch (IOException e){
+            System.err.println("Err" + e.getMessage());
         }
     }
 }
